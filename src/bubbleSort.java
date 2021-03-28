066715595c25bceb3128c0d03321b0e34ac09ac3
ss2261 <ss2261@kent.ac.uk>
2021-03-28 16:23:01 +0100

    bubble sort for array of 5 items completed

--- src/bubbleSort.java
+++ src/bubbleSort.java
@@ -18,9 +18,25 @@ public class bubbleSort {
 
 
     public static void bubbleSortAlg(int[] arrInput){
+        int temp1;
+        int temp2;
+        int[] newInputArr = new int[5];
+        for(int j=0; j<4;j++) { //number of compares is 4
+            for (int x = 0; x < arrInput.length - 1; x++) {
 
+                if (arrInput[x] > arrInput[x + 1]) {
 
+                    temp1 = arrInput[x + 1];//1
+                    temp2 = arrInput[x];//5
+
+                    arrInput[x] = temp1;// 1
+                    arrInput[x + 1] = temp2;//5
+
+                }
+            }
+        }
         System.out.println(Arrays.toString(arrInput));
+
 
     }
 }
