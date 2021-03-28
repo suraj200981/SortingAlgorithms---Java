093e5ba94aa5852d61a8efb4859ed71e1bef077f
ss2261 <ss2261@kent.ac.uk>
2021-03-28 14:09:38 +0100

    passing unsorted array to bubble sort method

--- src/bubbleSort.java
+++ src/bubbleSort.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+
 public class bubbleSort {
+
+
     public static void main(String[] args) {
+        int [] InputArr = new int[5];
+        InputArr[0] = 5;
+        InputArr[1] = 1;
+        InputArr[2] = 4;
+        InputArr[3] = 2;
+        InputArr[4] = 8;
+
+
+    bubbleSortAlg(InputArr);
+    }
+
+
+
+    public static void bubbleSortAlg(int[] arrInput){
+
+
+        System.out.println(Arrays.toString(arrInput));
 
     }
 }
