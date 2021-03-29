43f1454a06a31bc78b2df7d531fd143a9e9afb1a
ss2261 <ss2261@kent.ac.uk>
2021-03-29 20:48:55 +0100

    InsertionSort inner search loop almost complete
    returning values [1,2,4,8,3]

--- src/insertionSort.java
+++ src/insertionSort.java
@@ -17,26 +17,19 @@ public class insertionSort {
     }
 
     public static void selectionSortAlg(int[] arr) {
-        int beginning;
         int current;
-        int tempCurrent;
-        int[] newInputArr = new int[5];
+        int temp1 = 0;
+        for (int x = 0; x < arr.length - 1; x++) {//8
+                current = arr[x];//8
 
-        for (int x = 0; x < arr.length - 1; x++) {
-                beginning = arr[x];//8
-            if(arr[x]>arr[x+1]){
-                current = arr[x+1];
-                arr[x+1] =beginning;
-                beginning = current;
-                arr[x]= beginning;
-                //sorted
-            }
-            for(int j =x+1; j<arr.length-1; j++) {
-                if(arr[j]<beginning){
+            for(int j =x+1; j<arr.length-1; j++) {//2
 
-
+                if(current>arr[j]){
+                    temp1 = arr[j];
+                    arr[j] = current;//8
+                    arr[x] = temp1;//2
                 }
-
+                current = temp1;
             }
 
         }
