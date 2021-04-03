7fd4499121967af4c600e49688a8893365325d08
ss2261 <ss2261@kent.ac.uk>
2021-04-03 18:23:56 +0100

    Heap sort -  build max heap function is complete

--- src/heapSort.java
+++ src/heapSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class heapSort {
     public static void main(String[] args) {
         int[] InputArr = new int[6];
@@ -14,21 +16,33 @@ public class heapSort {
 
     }
 
-    public static void heapSortAlg(int [] arr){
+    public static void heapSortAlg(int[] arr) {
 
-        int temp1 = 0;
-        int temp2;
+        System.out.println("build max heap:");
+        System.out.println(Arrays.toString(buildMaxHeap(arr)));
+
+
+    }
+
+    public static int[] buildMaxHeap(int[] arrMaxHeap) {
+        int temp1 = 0;//store biggest element
+        int temp2 = arrMaxHeap[0]; // store first element in the array
+        int temp3 = 0;
+
 
         //First find largest in the heap
-        for(int x=0; x<arr.length-1; x++){
+        for (int x = 0; x < arrMaxHeap.length - 1; x++) {
 
-            if (arr[x]<arr[x+1]){
-                temp1 = arr[x+1];
+            if (arrMaxHeap[x] < arrMaxHeap[x + 1]) {
+                temp1 = arrMaxHeap[x + 1];//9
+                arrMaxHeap[0] = temp1;
+                temp3 = x + 1;
             }
-        }
-        System.out.println(temp1);
+        }//once 9 is found replace initial 9 pos with index 0 element
 
+        arrMaxHeap[temp3] = temp2;
 
+        return arrMaxHeap;
 
 
     }
